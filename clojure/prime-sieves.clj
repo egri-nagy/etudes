24d200612f1175fa056711da30036c4f8c66c1db
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-04-29 08:18:46 +1000

    added wheel-sieve

--- clojure/prime-sieves.clj
+++ clojure/prime-sieves.clj
@@ -17,3 +17,21 @@
       (if (.get bs p)
         (doseq [q (range (* p p) n p)] (.clear bs q))))
     bs))
+
+;; Taken from clojure.contrib.lazy-seqs
+; wheel-sieve cannot be written efficiently as a function, because
+; it needs to look back on the whole sequence.
+(def wheel-sieve
+  (concat
+   [2 3 5 7]
+   (lazy-seq
+    (let [primes-from
+          (fn primes-from [n [f & r]]
+            (if (some #(zero? (rem n %))
+                      (take-while #(<= (* % %) n) wheel-sieve))
+              (recur (+ n f) r)
+              (lazy-seq (cons n (primes-from (+ n f) r)))))
+          wheel (cycle [2 4 2 4 6 2 6 4 2 4 6 6 2 6  4  2
+                        6 4 6 8 4 2 4 2 4 8 6 4 6 2  4  6
+                        2 6 6 4 2 4 6 2 6 4 2 4 2 10 2 10])]
+      (primes-from 11 wheel)))))
