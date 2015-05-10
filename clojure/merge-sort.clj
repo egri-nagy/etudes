4a2d9f8464f4886ed7fe74216c2474c2aa4789db
Dersu Uzala <attila@egri-nagy.hu>
2015-05-10 19:42:54 +1000

    adding comments

--- clojure/merge-sort.clj
+++ clojure/merge-sort.clj
@@ -8,13 +8,16 @@
     (partition-all half coll)))
 
 (defn merge-sort
+  "sorting the given collection with merge-sort"
   [coll]
   (if (or (empty? coll) (= 1 (count coll)))
     coll
     (let [[l1 l2] (cut-in-half coll)]
+      ;recursive call
       (loop [r [] l1 (merge-sort l1) l2 (merge-sort l2)]
-        (cond (empty? l1) (into r l2)
-              (empty? l2) (into r l1)
-              :else (if (> 0 (compare (first l1) (first l2)))
+        ;merging
+        (cond (empty? l1) (into r l2) ;when l1 is exhausted
+              (empty? l2) (into r l1) ;when l2 is exhausted
+              :else (if (> 0 (compare (first l1) (first l2))) ;comparison
                       (recur (conj r (first l1)) (rest l1) l2)
                       (recur (conj r (first l2)) l1 (rest l2))))))))
