e6afd109bc438d7a31fc68d1b2685048ad651d44
Dersu Uzala <attila@egri-nagy.hu>
2015-05-12 20:44:06 +1000

    clojure quicksort added

--- clojure/sort.clj
+++ clojure/sort.clj
@@ -14,3 +14,13 @@
               :else (if (> 0 (compare (first l1) (first l2))) ;comparison
                       (recur (conj r (first l1)) (rest l1) l2)
                       (recur (conj r (first l2)) l1 (rest l2))))))))
+
+(defn quick-sort
+  "sorting the given collection with quick-sort (functional style)"
+  [coll]
+  (if (or (empty? coll) (= 1 (count coll)))
+    coll
+    (let [pivot (first coll)
+          greater (filter #(>= % pivot) (rest coll))
+          smaller (filter #(< % pivot) (rest coll))]
+      (concat (quick-sort smaller) [pivot] (quick-sort greater)))))
