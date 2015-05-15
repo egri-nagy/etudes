9e9d0c725b324abfa853f42c455343dce836491f
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-05-15 10:44:10 +1000

    hanoi towers

---
+++ clojure/hanoi.clj
@@ -0,0 +1,8 @@
+(defn towers-of-hanoi
+  ([numofdisks] (towers-of-hanoi numofdisks :rodA :rodB :rodC))
+  ([numofdisks from to via]
+   (if (= 1 numofdisks) (println (format "%s to %s" from to))
+       (do
+         (towers-of-hanoi (dec numofdisks) from via to)
+         (println (format "%s to %s" from to))
+         (recur (dec numofdisks) via to from)))))
