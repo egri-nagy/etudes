704031670b5860cb99e0c4f088b1e1a5a1bb1a67
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-05-26 11:10:10 +1000

    adding some comments

--- clojure/expt.clj
+++ clojure/expt.clj
@@ -1,4 +1,6 @@
-;; from SICP
+;; exponentiation in logarithmic number of steps, the actual number depends on
+;; the number of ones in the binary representation of n (from SICP)
+;; using *' to support arbitrary precision
 (defn fast-expt [b n]
   (cond (= n 0) 1
         (even? n) (#(*' % %) (fast-expt b (/ n 2)))
